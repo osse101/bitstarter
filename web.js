fecb18d20a45164bb2c4db1cd6057308f0e12cf3
osse101 <mikemck101@gmail.com>
2013-09-10 17:24:55 +0000

    Server listens on port 8080 now.  Added external scripts to index.html

--- web.js
+++ web.js
@@ -1,26 +1,17 @@
 var express = require('express');
 var fs = require('fs');
-
-
-
-var buffer;
-fs.readFile('index.html', 'utf8', function(err, data){
-    if(err){
-	return console.log(err);
-    }
-    console.log(data);
-    buffer = new Buffer(data, "utf-8");
-});
+var htmlfile = "index.html";
 
 
 var app = express.createServer(express.logger());
 
 
 app.get('/', function(request, response) {
-    response.send(buffer.toString("utf-8", 0));
+    var html = fs.readFileSync(htmlfile).toString();
+    response.send(html);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
